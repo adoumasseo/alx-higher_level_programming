40c3fdca86ae7f93468acc213f5143b625614910
adoumasseo <adoumasseo@gmail.com>
2024-01-03 22:20:46 +0100

    a function that checks for lowercase character. Fix ord(Ã) using

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     _c = ord(c)
-    if _c < ord(a) or _c > ord(z):
+    if _c < ord('a') or _c > ord('z'):
         return True
     else:
         return False
