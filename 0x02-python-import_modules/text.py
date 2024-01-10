39749bc80b44438e242f20f1d36efc33d7e05651
adoumasseo <adoumasseo@gmail.com>
2024-01-10 23:52:18 +0100

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x02-python-import_modules/text.py
+++ 0x02-python-import_modules/text.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
+import dis
 
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+# Votre bytecode complet
+bytecode_instructions = [
+    (0, 'LOAD_CONST', 1),
+    (3, 'LOAD_CONST', 2),
+    (6, 'IMPORT_NAME', 'magic_calculation_102'),
+    (9, 'IMPORT_FROM', 'add'),
+    (12, 'STORE_FAST', 'add'),
+    (15, 'IMPORT_FROM', 'sub'),
+    (18, 'STORE_FAST', 'sub'),
+    (21, 'POP_TOP', None),
+    (22, 'LOAD_FAST', 0),
+    (25, 'LOAD_FAST', 1),
+    (28, 'COMPARE_OP', 0),
+    (31, 'POP_JUMP_IF_FALSE', 94),
+    (34, 'LOAD_FAST', 'add'),
+    (37, 'LOAD_FAST', 0),
+    (40, 'LOAD_FAST', 1),
+    (43, 'CALL_FUNCTION', (2, 0)),
+    (46, 'STORE_FAST', 'c'),
+    (49, 'SETUP_LOOP', 38),
+    (52, 'LOAD_GLOBAL', 'range'),
+    (55, 'LOAD_CONST', 3),
+    (58, 'LOAD_CONST', 4),
+    (61, 'CALL_FUNCTION', (2, 0)),
+    (64, 'GET_ITER', None),
+    (65, 'FOR_ITER', 21),
+    (68, 'STORE_FAST', 'i'),
+    (71, 'LOAD_FAST', 'add'),
+    (74, 'LOAD_FAST', 'c'),
+    (77, 'LOAD_FAST', 'i'),
+    (80, 'CALL_FUNCTION', (2, 0)),
+    (83, 'STORE_FAST', 'c'),
+    (86, 'JUMP_ABSOLUTE', 65),
+    (89, 'POP_BLOCK', None),
+    (90, 'LOAD_FAST', 'c'),
+    (93, 'RETURN_VALUE', None),
+    (94, 'LOAD_FAST', 'sub'),
+    (97, 'LOAD_FAST', 0),
+    (100, 'LOAD_FAST', 1),
+    (103, 'CALL_FUNCTION', (2, 0)),
+    (106, 'RETURN_VALUE', None),
+    (107, 'LOAD_CONST', None),
+    (110, 'RETURN_VALUE', None),
+]
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
-    if sys.argv[2] == '+':
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+# Afficher le bytecode personnalisé
+for instruction in bytecode_instructions:
+    print(instruction[0], instruction[1], instruction[2])
