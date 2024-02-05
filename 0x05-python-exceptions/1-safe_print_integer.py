4e2a0c1c92ca10354111c44f52340d89e8a9f194
adoumasseo <adoumasseo@gmail.com>
2024-02-05 21:34:38 +0100

    Adding TypeError to except bloc

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
+    except (TypeError, ValueError):
         return False
     return True
