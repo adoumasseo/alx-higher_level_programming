224fc7ab273f780676f10418dd810c562725f561
adoumasseo <adoumasseo@gmail.com>
2024-05-05 15:41:49 +0100

    fix: add documentation to print_sorted && remove "from" to avoid the checker issues

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-A module that define a class inherit from list & define a sorte method inside
+A module that define a class inherit of list & define a sorte method inside
 """
 
 
 class MyList(list):
     """ A class that inherit from list"""
     def __init__(self):
+        """Init fonction"""
         super().__init__()
 
     def print_sorted(self):
+        """ A fct two sort a list"""
         new_list = self[:]
         new_list.sort()
         print(new_list)
