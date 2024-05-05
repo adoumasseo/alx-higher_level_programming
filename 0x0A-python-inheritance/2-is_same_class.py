38714d64239c9cb057fddeda1797cb990e3c2b92
adoumasseo <adoumasseo@gmail.com>
2024-05-05 17:26:19 +0100

    push: 2-is_same_class.py file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    a function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    """A fct that check a type of an object"""
+    return type(obj) == a_class
