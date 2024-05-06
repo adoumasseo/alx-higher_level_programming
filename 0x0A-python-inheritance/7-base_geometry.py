e8f77884a86f19b2dea224affc4ee521ff9b9945
adoumasseo <adoumasseo@gmail.com>
2024-05-06 14:22:25 +0100

    push: 7-base_geometry.py && 7-base_geometry.txt files

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    A module for an Geometry class
+"""
+
+
+class BaseGeometry:
+    """
+        Geometry Class
+    """
+    def area(self):
+        """
+            A fct to calcul area of a Geomotry figure
+            Args:
+                self: An instance of BaseGeometry
+            Returns:
+                Just Raise an Exception for now
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            Purpose is to validate value
+            Args:
+                self: an instance of BaseGeometry
+                name (string): Name of our value
+                value (int): value must be int
+            Returns:
+                Nothing just Validate value
+                and raise specific exception if sometimes gone wrong
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
