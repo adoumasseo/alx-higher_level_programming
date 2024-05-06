19907d0874c1db08e23b609fa068bb0b80f388f1
adoumasseo <adoumasseo@gmail.com>
2024-05-06 13:27:45 +0100

    push: 6-base_geometry.py file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    A module for an Geometry class
+"""
+
+
+class BaseGeometry:
+    """
+        Geometry Class
+    """
+    def area(self):
+        """
+            A fct to calcul area of a Geomotry figure
+            Args:
+                self: An instance of BaseGeometry
+            Returns:
+                Just Raise an Exception for now
+        """
+        raise Exception("area() is not implemented")
