c67ca31c4e50a57638d088c1b80d6e9d806282c8
adoumasseo <adoumasseo@gmail.com>
2024-05-06 14:24:55 +0100

    fix: 7-base_geometry.py if/else logics

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry:
                 Nothing just Validate value
                 and raise specific exception if sometimes gone wrong
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
