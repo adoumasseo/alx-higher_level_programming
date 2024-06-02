691574c734766ae9e3f25cc7de8f9cb7554a505b
adoumasseo <adoumasseo@gmail.com>
2024-06-02 10:31:09 +0100

    prints the full description of the table first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- In this one create a table first_table
+-- id(INT), name(VARCHAR(256)) Database name is pass as argument
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+);
