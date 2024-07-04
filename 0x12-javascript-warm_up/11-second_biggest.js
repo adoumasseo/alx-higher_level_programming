f9adb3f01abbde70f27889d992863cc481a120a3
adoumasseo <adoumasseo@gmail.com>
2024-07-04 15:41:49 +0100

    second bigest int fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 let greatest = parseInt(process.argv[2]);
-let secondGreatest = parseInt(process.argv[2]);
-const argc = process.argv;
-if (argc.length <= 2) {
+let Bgreatest = parseInt(process.argv[2]);
+const args = process.argv;
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (let i = 2; i < argc.length; i++) {
-    if (argc[i] > greatest) {
-      secondGreatest = greatest;
-      greatest = argc[i];
+  for (let i = 2; i < args.length; i++) {
+    if (args[i] > greatest) {
+      Bgreatest = greatest;
+      greatest = args[i];
     }
   }
-  console.log(secondGreatest);
+  console.log(Bgreatest);
 }
