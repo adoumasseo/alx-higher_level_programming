37c8776a5f4c9e9a4deeb37f926ebae154a462c1
adoumasseo <adoumasseo@gmail.com>
2024-07-04 15:48:45 +0100

    Second biggest number; second attemp to fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-let greatest = parseInt(process.argv[2]);
-let Bgreatest = parseInt(process.argv[2]);
-const args = process.argv;
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  for (let i = 2; i < args.length; i++) {
-    if (args[i] > greatest) {
-      Bgreatest = greatest;
-      greatest = args[i];
-    }
-  }
-  console.log(Bgreatest);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
